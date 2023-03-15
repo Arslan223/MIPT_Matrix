51e916a681027265873f8a902bc3fa9d09244f17
ars <khalilov.ash@phystech.edu>
2023-03-15 19:20:53 +0300

    upload matrix code

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <cassert>
+#include <climits>
+#include <exception>
+#include <fstream>
+#include <iostream>
+#include <vector>
+
+#include "matrix.h"
+
+int main() {
+  Matrix<3, 2> matr{{1, 2}, {1, 2}, {1, 2}};
+}
